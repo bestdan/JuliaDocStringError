74f2ff3d07d018faf4209373c6400499df4df910
Daniel P. Egan <dp.egan@gmail.com>
2021-08-23 15:16:31 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.18

---
+++ src/JuliaDocStringError.jl
@@ -0,0 +1,5 @@
+module JuliaDocStringError
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using JuliaDocStringError
+using Test
+
+@testset "JuliaDocStringError.jl" begin
+    # Write your tests here.
+end
